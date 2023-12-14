efe519337a9759e5148eba99315c975f2bbb1e3e
Stepan Martinovsky <46663905+crayonwow@users.noreply.github.com>
2023-12-14 18:30:28 +0100

    added rate limiter (#6)

--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -5,34 +5,36 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log/slog"
 	"mime/multipart"
 	"net/http"
 	"strings"
+	"time"
+
+	"golang.org/x/time/rate"
 )
 
 const apiUrl = "https://api.telegram.org/bot{{token}}/{{method_name}}"
 
-type method string
+const methodSendVideo = "sendVideo"
 
-func (m method) String() string { return string(m) }
-
-const methodSendVideo method = "sendVideo"
-
-func newUrl(token string, method method) string {
+func newUrl(token string, method string) string {
 	s := strings.Replace(apiUrl, "{{token}}", token, 1)
-	s = strings.Replace(s, "{{method_name}}", method.String(), 1)
+	s = strings.Replace(s, "{{method_name}}", method, 1)
 	return s
 }
 
 type Bot struct {
-	token  string
-	client *http.Client
+	token   string
+	client  *http.Client
+	limiter *rate.Limiter
 }
 
-func NewBot(cli *http.Client, token string) *Bot {
+func NewBot(cli *http.Client, token string, notifyEvery time.Duration) *Bot {
 	return &Bot{
-		token:  token,
-		client: cli,
+		token:   token,
+		client:  cli,
+		limiter: rate.NewLimiter(rate.Every(1*notifyEvery/1), 1),
 	}
 }
 
@@ -42,6 +44,29 @@ func (b *Bot) UploadVideo(
 	isSilent, hasSpoiler bool,
 	videoFile io.Reader,
 ) error {
+	return b.uploadVideo(ctx, chatID, message, b.isSilent(isSilent), hasSpoiler, videoFile)
+}
+
+func (b *Bot) isSilent(isSilent bool) bool {
+	if !isSilent {
+		isSilent = !b.limiter.Allow()
+	}
+	return isSilent
+}
+
+func (b *Bot) uploadVideo(
+	ctx context.Context,
+	chatID, message string,
+	isSilent, hasSpoiler bool,
+	videoFile io.Reader,
+) error {
+	slog.Info("uploading video",
+		"chatID", chatID,
+		"message", message,
+		"isSilent", isSilent,
+		"hasSpoiler", hasSpoiler,
+	)
+
 	r, w := io.Pipe()
 	m := multipart.NewWriter(w)
 
@@ -49,21 +74,16 @@ func (b *Bot) UploadVideo(
 		defer w.Close()
 		defer m.Close()
 
-		if err := m.WriteField("chat_id", chatID); err != nil {
-			w.CloseWithError(err)
-			return
-		}
-		if err := m.WriteField("caption", message); err != nil {
-			w.CloseWithError(err)
-			return
-		}
-		if err := m.WriteField("disable_notification", fmt.Sprintf("%t", isSilent)); err != nil {
-			w.CloseWithError(err)
-			return
-		}
-		if err := m.WriteField("has_spoiler", fmt.Sprintf("%t", hasSpoiler)); err != nil {
-			w.CloseWithError(err)
-			return
+		for k, v := range map[string]string{
+			"chat_id":              chatID,
+			"caption":              message,
+			"disable_notification": fmt.Sprintf("%t", isSilent),
+			"has_spoiler":          fmt.Sprintf("%t", hasSpoiler),
+		} {
+			if err := m.WriteField(k, v); err != nil {
+				w.CloseWithError(err)
+				return
+			}
 		}
 		part, err := m.CreateFormFile("video", "video.mp4")
 		if err != nil {
@@ -84,7 +104,7 @@ func (b *Bot) UploadVideo(
 		}
 	}()
 
-	req, err := http.NewRequest("POST", newUrl(b.token, methodSendVideo), r)
+	req, err := http.NewRequest(http.MethodPost, newUrl(b.token, methodSendVideo), r)
 	if err != nil {
 		return fmt.Errorf("failed to create request: %w", err)
 	}
